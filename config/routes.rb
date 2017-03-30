147b9e85029117de9fde2cb98a3868c7dfcd13e1
rymcmahon <toedragdangle9@gmail.com>
2017-03-30 14:26:51 -0500

    added api version one and built out leads controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/leads' => 'leads#index'
-  get '/leads/:id' => 'leads#show'
-  post '/leads' => 'leads#create'
+  namespace :api do
+    namespace :v1 do
+      get '/leads' => 'leads#index'
+      get '/leads/:id' => 'leads#show'
+      post '/leads' => 'leads#create'
+    end
+  end
 end
