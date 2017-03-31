a8a11b3aae8b81b0da6936cc4f2d8fb1afa33ea3
rymcmahon <toedragdangle9@gmail.com>
2017-03-31 09:40:51 -0500

    added created and update at

--- app/controllers/api/v1/leads_controller.rb
+++ app/controllers/api/v1/leads_controller.rb
@@ -19,8 +19,10 @@ class Api::V1::LeadsController < ApplicationController
       lead.city = params[:city]
       lead.state = params[:state]
       lead.zip = params[:zip]
+      lead.created_at = params[:created_at]
+      lead.updated_at = params[:updated_at]
     end
-    @lead.events.create(name: params[:name])
+    @lead.events.create(name: params[:name], created_at: params[:created_at], updated_at: params[:updated_at])
     render "show.json.jbuilder"
   end
 end
