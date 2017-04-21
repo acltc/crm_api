71c26784ea447a1accb0c476e5f6a8c8bbcf61b5
Jay Wengrow <jaywngrw@gmail.com>
2017-04-21 14:57:46 -0500

    fixed typo

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -2,6 +2,6 @@ class Lead < ApplicationRecord
   has_many :events
 
   def self.next
-    Lead.where(contacted: false).where(bad_number: false).where('created_at < ?', '2017-04-21 19:06:25').order(:created_at).last
+    Lead.where(contacted: false).where(bad_number: false).where('created_at > ?', '2017-04-21 19:06:25').order(:created_at).last
   end
 end
