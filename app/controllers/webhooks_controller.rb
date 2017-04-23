42158f395d6266a4a6694ab4095045a7a411f24c
Jay Wengrow <jaywngrw@gmail.com>
2017-04-23 17:24:07 -0500

    removed unneeded code

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -14,14 +14,6 @@ class WebhooksController < ApplicationController
   end
 
   def incoming_text
-    # twiml = Twilio::TwiML::Response.new do |r|
-    #     r.Message do |message|
-    #     # message.Body params['Body']
-    #     # message.Media "https://demo.twilio.com/owl.png"
-    #     # message.Media "https://demo.twilio.com/logo.png"
-    #     end
-    # end
-    # render xml: twiml.text
     @client = Twilio::REST::Client.new
     @client.messages.create(
       from: '+17734666919',
