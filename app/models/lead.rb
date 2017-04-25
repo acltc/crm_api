99b910e611d77a4765f356be4f2bdcb153037b8c
Jay Wengrow <jaywngrw@gmail.com>
2017-04-25 14:35:41 -0500

    updated lead.next algorithm

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -4,13 +4,7 @@ class Lead < ApplicationRecord
   before_save :standardize_phone
 
   def self.next
-    if Rails.env.development?
-      # In development, we deal with leads from all time:
-      return Lead.where(contacted: false).where(bad_number: false).where(exclude_from_calling: false).where("phone <> ''").order(:created_at).last
-    else
-      # In production, we are currently only dealing with leads from a certain date
-      return Lead.where(contacted: false).where(bad_number: false).where(exclude_from_calling: false).where("phone <> ''").where('created_at > ?', '2017-04-21 19:06:25').order(:created_at).last
-    end    
+    Lead.where(hot: true).where("phone <> ''").order(:created_at).last  
   end
 
   def process
