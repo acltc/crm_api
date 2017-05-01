2f93ba75884f5fdf1ea3ac2ec73397756eedbf5f
Jay Wengrow <jaywngrw@gmail.com>
2017-05-01 12:13:33 -0700

    removed puts

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -7,11 +7,8 @@ class Admin < ApplicationRecord
   has_many :daily_progress_logs
 
   def record_progress(lead)
-    puts "PROGRESS!"
     log = DailyProgressLog.find_or_create_by(admin_id: self.id, date: Date.today)
-    puts "PROGRESS!"
     log.increment(:processed)
-    puts "PROGRESS!"
     log.increment(:connects) if lead.connected
     log.increment(:sets) if lead.appointment_date
     log.save
