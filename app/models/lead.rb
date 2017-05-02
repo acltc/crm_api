e75c5d6214676e22cb8301e62468fe500a2370f9
Jay Wengrow <jaywngrw@gmail.com>
2017-05-02 09:32:09 -0700

    next algorithm for zev

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -6,8 +6,10 @@ class Lead < ApplicationRecord
   attr_accessor :call_mode
 
   def self.next(admin_email=nil)
-    if admin_email == "stefanie@anyonecanlearntocode.com" || admin_email == "rena@actualize.co"
-      return Lead.where(number_of_dials: 1).where(hot: false).where(exclude_from_calling: false).where(connected: false).where(connected: false).where(bad_number: false).where("phone <> ''").order(:updated_at).last
+    if admin_email == "rena@actualize.co"
+      return Lead.where(number_of_dials: 1).where(old_lead: false).where(hot: false).where(exclude_from_calling: false).where(connected: false).where(connected: false).where(bad_number: false).where("phone <> ''").order(:updated_at).last
+    elsif admin_email = "zev@actualize.co"
+      return Lead.where("number_of_dials < 2").where(old_lead: true).where(hot: false).where(exclude_from_calling: false).where(connected: false).where(connected: false).where(bad_number: false).where("phone <> ''").order(:updated_at).last
     else 
       # We first look for a hot lead. This is defined by a lead who was either never dialed (contacted) or someone who we dialed but never connected with and they triggered a new event since we last dialed them them:
       hot_lead = Lead.where(hot: true).where(exclude_from_calling: false).where(connected: false).where("phone <> ''").order(:updated_at).last
