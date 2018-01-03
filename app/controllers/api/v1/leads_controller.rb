dd2bc37aca36ca933b241b8bef92bcd2a315a5d5
Jay Wengrow <jaywngrw@gmail.com>
2018-01-03 12:46:26 -0600

    important to fix to make sure people who download curriculum first get a first name and phone number when they begin an application

--- app/controllers/api/v1/leads_controller.rb
+++ app/controllers/api/v1/leads_controller.rb
@@ -18,17 +18,18 @@ class Api::V1::LeadsController < ApplicationController
     # event they triggered. If they've triggered an event previously and already
     # become a lead in the past, we just record their new event.
     
-    @lead = Lead.find_or_create_by(email: params[:email]) do |lead|
-      lead.first_name = params[:first_name]
-      lead.last_name = params[:last_name]
-      lead.phone = params[:phone]
-      lead.ip = params[:ip]
-      lead.city = params[:city]
-      lead.state = params[:state]
-      lead.zip = params[:zip]
-      lead.created_at = params[:created_at]
-      lead.updated_at = params[:updated_at]
-    end
+    @lead = Lead.find_or_initialize_by(email: params[:email])
+    @lead.first_name = params[:first_name] if params[:first_name]
+    @lead.last_name = params[:last_name] if params[:last_name]
+    @lead.phone = params[:phone] if params[:phone]
+    @lead.ip = params[:ip] if params[:ip]
+    @lead.city = params[:city] if params[:city]
+    @lead.state = params[:state] if params[:state]
+    @lead.zip = params[:zip] if params[:zip]
+    @lead.created_at = params[:created_at] if params[:created_at]
+    @lead.updated_at = params[:updated_at] if params[:updated_at]
+    @lead.save
+
     @lead.events.create(name: params[:name], created_at: params[:created_at], updated_at: params[:updated_at])
     render "show.json.jbuilder"
   end
