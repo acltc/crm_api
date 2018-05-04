b33fac3dd4292fcf61e451d2137b95577e269b5a
Jay Wengrow <jaywngrw@gmail.com>
2018-05-03 23:26:31 -0500

    spam checker api token

--- app/controllers/api/v1/leads_controller.rb
+++ app/controllers/api/v1/leads_controller.rb
@@ -50,7 +50,7 @@ class Api::V1::LeadsController < ApplicationController
     # event they triggered. If they've triggered an event previously and already
     # become a lead in the past, we just record their new event.
     
-    check_for_spam_ip_address = Unirest.get("https://api.apility.net/badip/#{params[:ip]}").code
+    check_for_spam_ip_address = Unirest.get("https://api.apility.net/badip/#{params[:ip]}?token=#{ENV['SPAM_CHECKER_API']}").code
 
       @lead = Lead.find_or_initialize_by(email: params[:email])
       @lead.first_name = params[:first_name] if params[:first_name].present?
